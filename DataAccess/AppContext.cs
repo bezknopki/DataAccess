d6e3d0d9d7927cffe8d720019d2713e18767e870
Эрнест Терегулов <aim.exe210@gmail.com>
2023-06-14 12:37:47 +0300

    Add project files.

---
+++ DataAccess/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Objective> Objectives { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=ObjectivesDB; Trusted_Connection=True");
+        }
+    }
+}
---
+++ DataAccess/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataAccess.Entities;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using AppContext appContext = new AppContext();
+            appContext.Objectives.Add(new Objective { Name = "FirstObjective" });
+            appContext.Objectives.Add(new Objective { Name = "SecondObjective", Description = "Objective with descripton" });
+            appContext.SaveChanges();
+
+            Console.WriteLine("");
+        }
+    }
+}
